dc5fa6f262f9e7826a0b6ede6679861bd27049b6
macs1756 <kindrativmaksum312@gmail.com>
2023-11-03 20:54:21 +0200

    v3

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,12 +1,32 @@
 import Link from 'next/link';
 import { SiBigbasket } from 'react-icons/si'
+import { MdAccountCircle, MdFavorite } from 'react-icons/md'
+import { RiShoppingBasketFill } from 'react-icons/ri'
 
 const Header = () => {
   return (
-    <header className="bg-slate-500 w-full p-3">
+    <header className="bg-slate-500 w-full p-3 flex items-center justify-between">
           <Link href='/'>
-            <SiBigbasket fontSize='42px' />
+            <SiBigbasket fontSize='42px' color='#fff' />
           </Link>
+          <ul>
+            <li>test</li>
+          </ul>
+
+          <div className='text-[27px] text-[#fff] flex gap-3'>
+
+          <Link href='/login'>
+            <MdAccountCircle />
+          </Link>
+
+          <Link href='/favorite'>
+            <MdFavorite />
+          </Link>
+
+          <RiShoppingBasketFill className='cursor-pointer' />
+        
+
+          </div>
     </header>
   );
 };
